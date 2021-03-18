793e2f978cb856ebf0cd5a257c4f27601de25c9d
Giancarlo Durastanti <cvd2261@protonmail.com>
2021-03-17 22:52:13 -0400

    moved storybook samples and got links in navbar

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 const NavBarContainer = styled.div`
   display: flex;
   position: sticky;
   width: 100%;
-  height: 3rem;
+  height: 4rem;
   border-bottom: 1px solid #c7cccf;
-  transition: border 1s ease-in-out;
+  transition: border 150ms ease;
+  align-items: center;
 
   &:hover {
-    border-bottom-color: #bdbdbd;
+    border-bottom-color: #a4a9ab;
   }
+`;
+
+const NavLink = styled.div`
+  color: #373a3c;
+  padding: 1rem 1rem;
 `;
 
 const routes = ['Home', 'About', 'Manage'];
 
 const NavBar = () => {
-  return <NavBarContainer />;
+  return (
+    <NavBarContainer>
+      {routes.map((route) => (
+        <NavLink>{<Link href={`/${route.toLowerCase()}`}>{route}</Link>}</NavLink>
+      ))}
+    </NavBarContainer>
+  );
 };
 
 export default NavBar;
